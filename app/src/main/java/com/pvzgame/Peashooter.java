004899ac59a935573d0e9d41f3f8949027c11f51
TheoLN <theoliviusnatael@gmail.com>
2024-05-17 13:22:15 +0700

    add things

---
+++ app/src/main/java/com/pvzgame/Peashooter.java
@@ -0,0 +1,14 @@
+package com.pvzgame;
+
+public class Peashooter extends Plant {
+    @Override
+    public void createPlant() {
+        sethealth(100);
+        setcooldown(10);
+        setattackDamage(25);
+        setattackSpeed(4);
+        setrange(-1);
+        set
+    }
+
+}
