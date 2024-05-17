ed1f5238eb72b05d78ae55bbf70e4d4b6df5ee7a
ammarnfl <ammarrnfl@gmail.com>
2024-05-17 23:35:19 +0700

    nambahin atribut deck

--- app/src/main/java/com/pvzgame/Deck.java
+++ app/src/main/java/com/pvzgame/Deck.java
@@ -1,17 +1,52 @@
 package com.pvzgame;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.pvzgame.Plant.Plant;
 
 public class Deck {
-    private List<Plant> kumpulanPlant;
+    private final List<Plant> currentDeck;
     private Integer availableSlot;
 
-    public Deck(Inventory currentInventory){
-        this.kumpulanPlant = currentInventory.getPlants();
-        // Integer availableSlot = new Integer(10);
+    public Deck(){
+        currentDeck = new ArrayList<>();
+        availableSlot = 0;
     }
 
+    public List<Plant> getDeck(){
+        return currentDeck;
+    }
 
+    public boolean isDeckEmpty(){
+        return availableSlot == 0;
+    }
 
+    public boolean isDeckFull(){
+        return availableSlot == 6;
+    }
+
+    public void addPlant(Plant plant){
+        if (!isDeckFull()) {
+            currentDeck.add(plant);
+            availableSlot++;
+        } else {
+            System.out.println("Deck is full. Cannot add more plants.");
+        }
+    }
+
+    public void removePlant(){
+        if (!isDeckEmpty()) {
+            currentDeck.remove(availableSlot-1);
+            availableSlot--;
+        } else {
+            System.out.println("Deck is empty. Cannot remove plant.");
+        }
+    }
+
+    public void printDeck(){
+        System.out.println("List Plant di dalam Deck:");
+        for (int i = 0; i < availableSlot; i++) {
+            System.out.printf("%d. %s", i+1, currentDeck.get(i).getName());   
+        }
+    }
 }
